c6476a7201c5a8cc528e94cccc1ec387eeb91289
Samir <hlobhandari909@gmail.com>
2024-02-12 21:39:19 +0530

    header section , link component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,20 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp"
+import Header from "./components/Header";
 
 export default function App() {
   return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile />} />
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
 
-    </Routes>
+      </Routes>
+    </div>
   </BrowserRouter>;
 }
