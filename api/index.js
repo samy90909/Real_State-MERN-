dbe7f0ce39a9e15f3d3ea101c2af9f24ff6a5a4a
Samir <hlobhandari909@gmail.com>
2024-02-13 16:25:25 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,27 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
+
+
+
+if (!process.env.MONGO) {
+    console.error('MongoDB connection string not provided. Exiting...');
+    process.exit(1);
+}
+//proimise funciton in js 
+mongoose.connect("mongodb+srv://hlobhandari909:hlobhandari909@mernstate.zdis2ir.mongodb.net/mernstate?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+        process.exit(1);
+    });
+
+
+
 
 app.listen(3000, () => {
     console.log("Server is running at port 3000");
